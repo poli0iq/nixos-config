7616021edab306e60718e492f6336ec133448678
Polina Vishneva <poli@0iq.dev>
2025-05-30 21:34:13 +0300

    secrets: update homura key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   poli-sayaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGZKFPQNRf1H/4k0wjCMiATRBQ1BoEYPSFX6EhGInWw poli@sayaka";
-  poli-homura = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPQ4KzhM6goZNj0dlFetShmXWc3yrdS1ohVMG6ton9P poli@homura";
+  poli-homura = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNOTz1K4rWBIMQ/FjWY6/CgUm/doRKs9w6MB2khUtnm poli@homura";
   users = [ poli-sayaka poli-homura ];
 
   sayaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKFarrIP0OqMflQwktGYk084TzztWipdWtgMTPv5+cIR";
