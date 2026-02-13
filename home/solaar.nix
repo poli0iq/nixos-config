953a1dd22f04e2cf970c54877b026ea5e84ca23e
Polina Vishneva <poli@0iq.dev>
2026-02-13 13:53:09 +0100

    home: solaar: improve readability and add user service

--- home/solaar.nix
+++ home/solaar.nix
@@ -1,33 +1,46 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  mkSolaarKeybinding =
+    key: action:
+    lib.generators.toYAML { } [
+      {
+        Key = [
+          key
+          "pressed"
+        ];
+      }
+      {
+        KeyPress = [
+          action
+          "click"
+        ];
+      }
+    ];
+
+  concatYamlDocuments = documents: builtins.concatStringsSep "\n---\n" documents;
+in
 {
-  xdg.configFile."solaar/rules.yaml".text = builtins.concatStringsSep "\n---\n" [
-    (pkgs.lib.generators.toYAML { } [
-      {
-        Key = [
-          "Screen Capture"
-          "pressed"
-        ];
-      }
-      {
-        KeyPress = [
-          "Print"
-          "click"
-        ];
-      }
-    ])
-    (pkgs.lib.generators.toYAML { } [
-      {
-        Key = [
-          "Mute Microphone"
-          "pressed"
-        ];
-      }
-      {
-        KeyPress = [
-          "XF86_AudioMicMute"
-          "click"
-        ];
-      }
-    ])
+  xdg.configFile."solaar/rules.yaml".text = concatYamlDocuments [
+    (mkSolaarKeybinding "Screen Capture" "Print")
+    (mkSolaarKeybinding "Mute Microphone" "XF86_AudioMicMute")
   ];
+
+  systemd.user.services.solaar = {
+    Unit = {
+      Description = "Logitech device settings manager";
+      PartOf = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.solaar}/bin/solaar --window hide";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
