6f1e75c86b2a8dfdd148ef52546d8f2f04683d80
Polina Vishneva <poli@0iq.dev>
2026-03-16 10:04:46 +0100

    home: starship: Drop unused gruvbox_dark palette

--- home/starship/default.nix
+++ home/starship/default.nix
@@ -41,19 +41,6 @@
         color_yellow = "bright-yellow";
       };
 
-      palettes.gruvbox_dark = {
-        color_fg0 = "#fbf1c7";
-        color_bg1 = "#3c3836";
-        color_bg3 = "#665c54";
-        color_blue = "#458588";
-        color_aqua = "#689d6a";
-        color_green = "#98971a";
-        color_orange = "#d65d0e";
-        color_purple = "#b16286";
-        color_red = "#cc241d";
-        color_yellow = "#d79921";
-      };
-
       username = {
         show_always = false;
         style_user = "bg:color_orange fg:color_fg0";
