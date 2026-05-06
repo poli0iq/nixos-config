da3fbdf355eeec2ce0a79eae0e0e6672e2e2517a
Polina Vishneva <poli@0iq.dev>
2026-05-07 00:40:05 +0200

    home: gnome: Don't enable night light

--- home/gnome/default.nix
+++ home/gnome/default.nix
@@ -56,10 +56,6 @@
       ];
     };
 
-    "org/gnome/settings-daemon/plugins/color" = {
-      night-light-enabled = true;
-    };
-
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Super>t";
       name = "Terminal";
