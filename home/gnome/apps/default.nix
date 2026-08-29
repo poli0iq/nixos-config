68275200e6f2fc66435a2c1a831b0c052c85eefe
Polina Vishneva <poli@0iq.dev>
2026-08-29 20:11:29 +0200

    home/gnome/apps: Replace geary with stamp

--- home/gnome/apps/default.nix
+++ home/gnome/apps/default.nix
@@ -21,49 +21,10 @@
     gradia
     # Pomodoro timer
     gnome-solanum
-    # Email client (I wish there was a better one)
-    geary
+    # Modern Evolution frontend
+    stamp
     # Manga reader
     komikku
-  ];
-
-  # TODO: remove once upgraded upstream
-  nixpkgs.overlays = [
-    (final: prev: {
-      geary = prev.geary.overrideAttrs (old: {
-        version = "0-unstable-2025-12-15";
-
-        src = pkgs.fetchFromGitLab {
-          domain = "gitlab.gnome.org";
-          owner = "GNOME";
-          repo = "geary";
-          rev = "be3ed978e2ad77c1197ccb5a6e887ffaeec12057";
-          hash = "sha256-6iOv+DOxfWVojXrsH4tV3yL9GUTVvWFOp2SErcWhtW8=";
-        };
-
-        nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.cmake ];
-
-        buildInputs =
-          (lib.lists.subtractLists (with pkgs; [
-            gcr
-            gtk3
-            webkitgtk_4_1
-          ]) old.buildInputs)
-          ++ (with pkgs; [
-            gtk4
-            webkitgtk_6_0
-            gcr_4
-            libspelling
-            libadwaita
-            libpeas2
-          ]);
-
-        postPatch = lib.replaceString ''
-          substituteInPlace meson.build \
-            --replace-fail "appstream_glib = dependency('appstream-glib', version: '>=0.7.10')" ""
-        '' "" old.postPatch;
-      });
-    })
   ];
 
   xdg.mimeApps = {
